49de6415d1ad0f20029dd751a38179b3325a0028
diyaprajapati <diyabprajapati2005@gmail.com>
2024-01-27 13:21:36 +0530

    Change website to work according to their size

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ function toggleTheme() {
     document.body.classList.toggle('dark-theme');
 
 }
+/*
+function toggleMobileMenu() {
+    var mobileMenu = document.querySelector('.mobile-menu');
+    var pagesLinks = document.querySelector('.pages-links');
+
+    mobileMenu.classList.toggle('visible');
+    pagesLinks.classList.toggle('visible');
+}
+*/
+function toggleMobileMenu() {
+    var dropdownMenu = document.querySelector('.dropdown-menu');
+
+    dropdownMenu.classList.toggle('visible');
+}
