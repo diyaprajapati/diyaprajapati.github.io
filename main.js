9712560c10651ad54899b398c0ba06b283e8a8d1
diyaprajapati <diyabprajapati2005@gmail.com>
2024-02-10 20:49:05 +0530

    add darktheme on load

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+//local storage theme store
+//local storage theme get
+function loadThisThing() {
+  if(localStorage.getItem("dark") === "true") {
+    document.body.classList.add("dark-theme");
+  }
+}
+
+//check which theme is in local
+
+
 function toggleTheme() {
     document.body.classList.toggle('dark-theme');
+
+    if(document.body.classList.contains("dark-theme")) {
+      localStorage.setItem("dark", "true");
+    }
+    else {
+      localStorage.setItem("dark","false");
+    }
+
     let sbarBtn = document.querySelector(".menu-btn-sidebar");
-    sbarBtn.classList.toggle(".menu-btn-sidebar-dark");
 }
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
