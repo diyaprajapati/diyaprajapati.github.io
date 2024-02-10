f498469feb16c9da26326cf7dc6b5b52d5e9345a
diyaprajapati <diyabprajapati2005@gmail.com>
2024-02-10 17:38:04 +0530

    add hamburger

--- main.js
+++ main.js
@@ -3,17 +3,15 @@ function toggleTheme() {
     document.body.classList.toggle('dark-theme');
 
 }
-/*
-function toggleMobileMenu() {
-    var mobileMenu = document.querySelector('.mobile-menu');
-    var pagesLinks = document.querySelector('.pages-links');
-
-    mobileMenu.classList.toggle('visible');
-    pagesLinks.classList.toggle('visible');
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'block';
+    const btn = document.querySelector(".menu-btn-sidebar");
+  btn.style.display = "none";
 }
-*/
-function toggleMobileMenu() {
-    var dropdownMenu = document.querySelector('.dropdown-menu');
-
-    dropdownMenu.classList.toggle('visible');
+function hideSidebar() {
+    const hidebar = document.querySelector('.sidebar');
+    hidebar.style.display = 'none';
+    const btn = document.querySelector(".menu-btn-sidebar");
+  btn.style.display = "block";
 }
